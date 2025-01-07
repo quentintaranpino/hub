18fab4b834d09ada0710fba1954ef1d45b1167fa
René Aaron <rene@twentyuno.net>
2025-01-07 17:40:15 +0100

    feat: internal app (wip)

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -35,6 +35,7 @@ import { OpeningAutoChannel } from "src/screens/channels/auto/OpeningAutoChannel
 import { FirstChannel } from "src/screens/channels/first/FirstChannel";
 import { OpenedFirstChannel } from "src/screens/channels/first/OpenedFirstChannel";
 import { OpeningFirstChannel } from "src/screens/channels/first/OpeningFirstChannel";
+import { Bitrefill } from "src/screens/internal-apps/Bitrefill";
 import { BuzzPay } from "src/screens/internal-apps/BuzzPay";
 import { SimpleBoost } from "src/screens/internal-apps/SimpleBoost";
 import { UncleJim } from "src/screens/internal-apps/UncleJim";
@@ -250,6 +251,10 @@ const routes = [
           {
             path: "zapplanner",
             element: <ZapPlanner />,
+          },
+          {
+            path: "bitrefill",
+            element: <Bitrefill />,
           },
         ],
       },
